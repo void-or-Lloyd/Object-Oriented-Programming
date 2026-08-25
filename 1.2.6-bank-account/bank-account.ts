cbb00a55028984270f6f6367d94af4daa7e7c57d
void-or-Lloyd <tobias.bjorklund08@gmail.com>
2026-08-25 13:00:52 +0300

    com4

---
+++ 1.2.6-bank-account/bank-account.ts
@@ -0,0 +1,30 @@
+export default class BankAccount {
+    accountHolder: string
+    balance: number
+
+    constructor(accountHolder: string, balance: number) {
+        this.accountHolder = accountHolder
+        this.balance = balance
+    }
+
+    deposit(amount: number): void {
+        if (amount > 0) this.balance += amount
+        else console.log("You can't deposit a negative amount!")
+    }
+
+    withdraw(amount: number): void {
+        if (amount > 0) {
+            if (amount < this.balance || amount == this.balance) this.balance -= amount 
+            else console.log("You can't withdraw more than your current balance!")
+        }
+        else console.log("Your can't withdraw a negative amount!")
+    }
+
+    getBalance(): number {return this.balance}
+
+    showInfo(): void {
+        console.log("__________________________")
+        console.log("Account holder: " + this.accountHolder)
+        console.log("Balance: " + this.balance + " €", `\n`)
+    }
+}
